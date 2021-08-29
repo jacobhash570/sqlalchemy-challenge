257294c5b0de668ab9e603a27c65f9ad648f0e89
Jacob <jacob.hash570@gmail.com>
2021-08-29 18:42:50 -0400

    Precipitation route.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import numpy as np
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
 
 
 engine  = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -16,6 +19,16 @@ app = Flask(__name__)
 @app.route("/")
 def Home():
     return ("Welcome to the API!")
+
+@app.route("/api/v1.0//precipitation")
+def precipitation():
+        last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+        last_date = dt.datetime.strptime(last_date[0], '%Y-%m-%d')
+        query_date = dt.date(last_date.year -1, last_date.month, last_date.day)
+        sel = [Measurement.date, Measurement.prcp]
+        query_result = session.query(*sel).filter(Measurement.date >= query_date).all()
+        precipitation_data_list = dict(query_result)
+        return jsonify(precipitation_data_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
