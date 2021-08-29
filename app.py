21a953afdb137839ac83ff6f6d476fe04cf16765
Jacob <jacob.hash570@gmail.com>
2021-08-29 18:17:25 -0400

    Engine added.

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import numpy as np
-
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
+
+
+engine  = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect = True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+app = Flask(__name__)
+@app.route("/")
+def Home():
+    return ("Welcome to the API!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
